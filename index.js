65047fbd95b941e1fbf4dee9948240c8576cc8ee
lizardlynx <gorbunova.elizaveta@gmail.com>
2024-09-14 15:38:14 +0300

    try add host

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ds = require('./server/database/databaseService.js');
 
 const app = build({ logger: true });
 const start = async () => {
-  await app.listen({ port: process.env.FASTIFY_PORT });
+  await app.listen({ port: process.env.FASTIFY_PORT, host: '0.0.0.0' });
   await app.ready();
 
   ds.init(app);
