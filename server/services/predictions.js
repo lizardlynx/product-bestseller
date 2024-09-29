ea920d82c1a3f8c1f030c493d3799bb36b9c9b0c
lizardlynx <gorbunova.elizaveta@gmail.com>
2024-09-29 21:05:14 +0300

    create more prediction functions

--- server/services/predictions.js
+++ server/services/predictions.js
@@ -1,16 +1,90 @@
 class Predictions {
-  sma(points, period) {
+  sma(points, period = 8) {
     const result = [];
     for (let i = points.length - 1; i >= period - 1; i--) {
-      const periodPoints = points.slice(i-period + 1, i + 1);
+      const periodPoints = points.slice(i - period + 1, i + 1);
       const sum = periodPoints.reduce((acc, curr) => acc + curr[1], 0);
-      result.push([points[i][0], sum/period]);
+      result.push([points[i][0], sum / period]);
     }
     return result.reverse();
   }
 
-  ema(points, period) {
+  ema(points, period = 8) {
     const result = [];
+    if (points.length < period) return [];
+    const smaPointsSet = points.slice(0, period);
+    let prevEma = this.sma(smaPointsSet, period)[0][1];
+    const multiplier = 2 / (points.length + 1);
+    for (let i = period; i < points.length; i++) {
+      prevEma = points[i][1] * multiplier + prevEma * (1 - multiplier);
+      result.push([points[i][0], +prevEma.toFixed(2)]);
+    }
+    return result;
+  }
+
+  rsi(points, period = 14) {
+    const result = [];
+    for (let i = period - 1; i < points.length; i++) {
+      const pointsPrices = points
+        .slice(i - period + 1, i + 1)
+        .map(([date, price]) => price);
+      const avgGain =
+        pointsPrices.reduce((acc, curr, currIndex) => {
+          if (currIndex == 0) return acc;
+          acc += Math.max(0, curr - pointsPrices[currIndex - 1]);
+          return acc;
+        }, 0) / pointsPrices.length;
+
+      const avgLoss =
+        pointsPrices.reduce((acc, curr, currIndex) => {
+          if (currIndex == 0) return acc;
+          acc += Math.max(0, pointsPrices[currIndex - 1] - curr);
+          return acc;
+        }, 0) / pointsPrices.length;
+
+      const rsi = +(100 - (100/(1 + avgGain/avgLoss))).toFixed(2);
+      result.push([points[i][0], rsi]);
+    }
+    return result;
+  }
+
+  leastSquares(points, period = 14) {
+    const results = [];
+    const n = points.length;
+    let sumY = 0;
+    let sumX = 0;
+    let sumXY = 0;
+    let sumXSq = 0;
+
+    for (let i = 0; i < points.length; i++) {
+      sumY += points[i][1];
+      sumX += points[i][0];
+      sumXY += points[i][0]*points[i][1];
+      sumXSq += points[i][0] * points[i][0];
+    }
+
+    const m = (n*sumXY - sumX*sumY)/(n*sumXSq - sumX*sumX);
+    const b = (sumY - m * sumX)/n;
+    for (let i = 0; i < points.length; i++) {
+      const x = points[i][0];
+      const y = m*x+b;
+      results.push([x, y])
+    }
+    return results;
+  }
+
+  // need to find best suited function by interpolation and extend it
+  linearExtrapolation(points, period = 14) {
+    const result = [];
+    const leastSquaresSet = this.leastSquares(points, period);
+    const [set1, set2] = leastSquaresSet.slice(-2);
+    console.log(set1, set2);
+    const k =  ((set2[1] - set1[1]) / (set2[0] - set1[0]));
+    for (let i = 0; i < period; i++) {
+      const x = points[points.length - 1][0] + 86400000*(i + 1)
+      const y = set1[1] + ( x - set1[1])* k;
+      result.push(x, y);
+    }
   }
 }
 
--- server/tests/predictions.test.js
+++ server/tests/predictions.test.js
@@ -23,4 +23,106 @@ describe('predictionService', () => {
       [6, 55.4],
       [8, 57.2]]);
   });
+
+  it('should correctly count ema for price range', () => {
+    const points = [
+      [123, 50],
+      [321, 57],
+      [1, 58],
+      [5, 53],
+      [4, 55],
+      [0, 49],
+      [7, 56],
+      [3, 54],
+      [6, 63],
+      [8, 64],
+    ];
+    const result = predictionService.ema(points, 5);
+    expect(result).toEqual([
+      [0, 53.58],
+      [7, 54.02],
+      [3, 54.02],
+      [6, 55.65],
+      [8, 57.17]]);
+  });
+
+  it('should correctly count rsi for price range', () => {
+    const points = [
+      [123, 50],
+      [321, 57],
+      [1, 58],
+      [5, 53],
+      [4, 55],
+      [0, 49],
+      [7, 56],
+      [3, 54],
+      [6, 63],
+      [8, 64],
+    ];
+    const result = predictionService.rsi(points, 5);
+    expect(result).toEqual([
+      [4, 66.67],
+      [0, 21.43],
+      [7, 45],
+      [3, 52.94],
+      [6, 66.67],
+      [8, 89.47]]);
+  });
+
+  it('should correctly count least squares for price range', () => {
+    const points = [
+      [1, 50],
+      [2, 57],
+      [3, 58],
+      [4, 53],
+      [5, 55],
+      [6, 49],
+      [7, 56],
+      [8, 54],
+      [9, 63],
+      [10, 64],
+    ];
+    const result = predictionService.leastSquares(points);
+    expect(result).toEqual([
+      [1, 51.78181818181818],
+      [2, 52.696969696969695],
+      [3, 53.61212121212121],
+      [4, 54.52727272727273],
+      [5, 55.442424242424245],
+      [6, 56.35757575757576],
+      [7, 57.27272727272727],
+      [8, 58.18787878787879],
+      [9, 59.1030303030303],
+      [10, 60.018181818181816],  
+    ]);
+  });
+
+  it('should correctly count linear extrapolation for price range', () => {
+    // todo write tests
+    const points = [
+      [1, 50],
+      [2, 57],
+      [3, 58],
+      [4, 53],
+      [5, 55],
+      [6, 49],
+      [7, 56],
+      [8, 54],
+      [9, 63],
+      [10, 64],
+    ];
+    const result = predictionService.linearExtrapolation(points);
+    expect(result).toEqual([
+      [1, 51.78181818181818],
+      [2, 52.696969696969695],
+      [3, 53.61212121212121],
+      [4, 54.52727272727273],
+      [5, 55.442424242424245],
+      [6, 56.35757575757576],
+      [7, 57.27272727272727],
+      [8, 58.18787878787879],
+      [9, 59.1030303030303],
+      [10, 60.018181818181816],  
+    ]);
+  });
 });
